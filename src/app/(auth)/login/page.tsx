1aef8ada5d67c5ee5f23c48c651e90174abe5511
Sahil Kamila <sahilkamila143@gmail.com>
2025-12-06 12:33:25 +0530

    feat: Implement the client side login & register logic.

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { useAuthStore } from '@/store/Auth'
+import React from 'react'
+
+const Loginpage = () => {
+    const {login} = useAuthStore()
+    const [isLoading, setIsLoading] = React.useState(false)
+    const [error, setError] = React.useState("")
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        //collect data
+        const formData = new FormData(e.currentTarget)
+        const email = formData.get("email")
+        const password = formData.get("password")
+
+        //validation
+        if (!email || !password) {
+            setError(() => "Please fill out all fields.")
+            return
+        }
+
+        setIsLoading(() => true)
+        setError(() => "")
+
+        //login => store
+        const loginResponse = await login(email.toString(), password.toString());
+        if (loginResponse.error) {
+            setError(() => loginResponse.error!.message)
+        }
+
+        setIsLoading(() => false)
+
+    }
+
+    return (
+        <div>
+        LoginPage
+        </div>
+    )
+}
+
+export default Loginpage
---
+++ src/app/api/answer/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
